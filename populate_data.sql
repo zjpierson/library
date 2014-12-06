b1ca3f993b1321b31f04dec2944314971993d312
turkk33 <kayhan.karatekeli@gmail.com>
2014-12-05 17:57:29 -0700

    adding the populate tables script

---
+++ populate_data.sql
@@ -0,0 +1,47 @@
+insert into AUTHOR values( 'Name1', 1);
+insert into AUTHOR values( 'Name2', 2);
+insert into AUTHOR values( 'Name3', 3);
+insert into AUTHOR values( 'Name4', 4);
+insert into AUTHOR values( 'Name5', 5);
+insert into AUTHOR values( 'Name6', 6);
+insert into AUTHOR values( 'Name7', 7);
+
+insert into BOOK values( 1, 'Title1', 1000, 1);
+insert into BOOK values( 2, 'Title2', 1001, 2);
+insert into BOOK values( 3, 'Title3', 1002, 3);
+insert into BOOK values( 4, 'Title4', 1003, 4);
+insert into BOOK values( 5, 'Title5', 1004, 5);
+insert into BOOK values( 6, 'Title6', 1005, 6);
+insert into BOOK values( 7, 'Title7', 1006, 7);
+
+insert into COPYBOOK values( 1, 1, 1, 10);
+insert into COPYBOOK values( 2, 2, 2, 20);
+insert into COPYBOOK values( 3, 3, 3, 30);
+insert into COPYBOOK values( 4, 4, 4, 40);
+insert into COPYBOOK values( 5, 5, 5, 50);
+insert into COPYBOOK values( 6, 6, 6, 60);
+insert into COPYBOOK values( 7, 7, 7, 70);
+
+insert into LIBRARY values( 1, 'LibName1', 'Loc1', 100);
+insert into LIBRARY values( 2, 'LibName2', 'Loc2', 100);
+insert into LIBRARY values( 3, 'LibName3', 'Loc3', 100);
+insert into LIBRARY values( 4, 'LibName4', 'Loc4', 100);
+insert into LIBRARY values( 5, 'LibName5', 'Loc5', 100);
+insert into LIBRARY values( 6, 'LibName6', 'Loc6', 100);
+insert into LIBRARY values( 7, 'LibName7', 'Loc7', 100);
+
+insert into LOAN values( 1, 1, 1, '1990-01-01', '1990-02-02');
+insert into LOAN values( 2, 2, 2, '1991-01-01', '1991-02-02');
+insert into LOAN values( 3, 3, 3, '1992-01-01', '1992-02-02');
+insert into LOAN values( 4, 4, 4, '1993-01-01', '1993-02-02');
+insert into LOAN values( 5, 5, 5, '1994-01-01', '1994-02-02');
+insert into LOAN values( 6, 6, 6, '1995-01-01', '1995-02-02');
+insert into LOAN values( 7, 7, 7, '1996-01-01', '1996-02-02');
+
+insert into PATRON values( 1, 'Name1', 'Type1');
+insert into PATRON values( 2, 'Name2', 'Type2');
+insert into PATRON values( 3, 'Name3', 'Type3');
+insert into PATRON values( 4, 'Name4', 'Type4');
+insert into PATRON values( 5, 'Name5', 'Type5');
+insert into PATRON values( 6, 'Name6', 'Type6');
+insert into PATRON values( 7, 'Name7', 'Type7');
